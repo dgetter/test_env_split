b97175f0dd60a84065d98e6d419ed48f08e84b76
dgetter <158026671+dgetter@users.noreply.github.com>
2024-03-17 10:54:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def main():
     return {"message": "hello from main"}
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
